b3961ef9dda8e1d81b460609906aad49e7c8ff3f
Garavirod <rodrigogarciaavila26@gmail.com>
2022-06-20 21:15:25 -0500

    project was inicialized

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
+import HomeScreen from './screens/HomeScreen';
 export default function App() {
   return (
+    <Provider store={store}>
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <HomeScreen/>
     </View>
+    </Provider>
   );
 }
 
